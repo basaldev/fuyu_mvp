2a675d79cc32cf89831e8d5f53cf3152fc29d56b
Adam Munoz <adam@exolever.com>
2019-01-17 15:35:01 +0900

    Drag and drop

--- src/fuyu_mvp/components/missing_pool.cljs
+++ src/fuyu_mvp/components/missing_pool.cljs
@@ -1,6 +1,12 @@
-(ns fuyu_mvp.components.missing-pool)
+(ns fuyu_mvp.components.missing-pool
+  (:require [goog.style :as style]
+            [goog.object :as object]))
+
+(defn drag-start [ev]
+  (println "drag-start")
+  (style/setOpacity (object/get ev "target") 0.05))
 
 (defn main [missing-letters]
   [:div {:class "row"}
     (map (fn [letter]
-          [:span letter]) missing-letters)])
+          [:span {:draggable true :on-drag-start drag-start} letter]) missing-letters)])
